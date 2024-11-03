c479a043d916e80f68ce93c60d09f978346b00ce
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-11-03 06:38:28 +0530

    create rest controller and define /courses endpoint

--- src/main/java/com/vaibhav/restspringboot/CourseController.java
+++ src/main/java/com/vaibhav/restspringboot/CourseController.java
@@ -1,4 +1,22 @@
 package com.vaibhav.restspringboot;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class CourseController {
+    /* /courses
+    * Course : id, name, author */
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+                new Course(1, "Spring and Spring Boot", "in28minutes"),
+                new Course(2, "Java Masterclass", "Tim Bachulka"),
+                new Course(3, "Scam Course", "Fraud Academy")
+        );
+    }
 }
